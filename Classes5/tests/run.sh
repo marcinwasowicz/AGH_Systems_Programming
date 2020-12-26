26b21c0174ef508c4060ec120d7d46e46e9bc8db
Marcin Wasowicz <marcinwasowicz@pc7.home>
2020-12-26 21:53:23 +0100

    polishing testing script

--- Classes5/tests/run.sh
+++ Classes5/tests/run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-buf_1="$(printf 'A%.0s' {1..100000})"
-buf_2="$(printf 'E%.0s' {1..100000})"
-buf_3="$(printf 'O%.0s' {1..100000})"
+buf_1="$(printf 'A%.0s' {1..10000})"
+buf_2="$(printf 'E%.0s' {1..10000})"
+buf_3="$(printf 'O%.0s' {1..10000})"
 
-(echo ${buf_1} > /dev/linked &)
-(echo ${buf_2} > /dev/linked &)
-(echo ${buf_3} > /dev/linked &)
+(echo "${buf_1}" > /dev/linked &)
+(echo "${buf_2}" > /dev/linked &)
+(echo "${buf_3}" > /dev/linked &)
 
-for i in `seq 30`
+for _ in $(seq 30)
 do
-    (cat /dev/linked > foo.txt)
-    (sleep 5)
+    (< /dev/linked tr -d -c "A" | wc -c)
+    (< /dev/linked tr -d -c "E" | wc -c)
+    (< /dev/linked tr -d -c "O" | wc -c)
+    (sleep 1)
 done
