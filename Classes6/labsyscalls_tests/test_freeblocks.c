977f1efe17cbfa86042bc75693b3287a1bcd3ec9
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-01-16 21:53:58 +0100

    implemented freeblocks syscall tests

---
+++ Classes6/labsyscalls_tests/test_freeblocks.c
@@ -0,0 +1,25 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+
+#define freeblocks_syscall 444
+
+int main(int argc, char* argv[]){
+    char* file_path = argv[1];
+    u_int64_t freeblocks_count;
+
+    long result = syscall(freeblocks_syscall, file_path, &freeblocks_count);
+
+    if(result){
+        printf("Call unsuccesfull, see dmesg for details\n");
+    }
+    else{
+        printf("Free blocks count obtained for given path is: %lld\n", freeblocks_count);
+    }
+
+    return 0;
+}
