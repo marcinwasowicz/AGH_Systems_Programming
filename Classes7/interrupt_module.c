4275e86a28d993a64fed3962a0e528e14353af0b
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-01-30 13:49:23 +0100

    implemented task related to tasklets

--- Classes7/interrupt_module.c
+++ Classes7/interrupt_module.c
@@ -7,9 +7,16 @@ MODULE_LICENSE("GPL");
 int handler_cookie;
 int net_dev_irq_no = 19;
 const char* net_dev_name = "ens33";
+struct tasklet_struct my_tasklet;
+unsigned long tasklet_data = 10;
+
+void tasklet_handler(unsigned long data){
+    printk("Tasklet executed with data: %ld\n", data);
+}
 
 irqreturn_t handler(int interrupt_no, void* cookie){
     printk("NIC device sent interrupt\n");
+    tasklet_schedule(&my_tasklet);
     return IRQ_NONE;
 }
 
@@ -19,11 +26,15 @@ static int __init interrupt_init(void){
     if(result){
         printk("Unable to register interrupt handler for NIC device\n");
     }
+    else{
+        tasklet_init(&my_tasklet, tasklet_handler, tasklet_data);
+    }
 
     return result;
 }
 
 static void __exit interrupt_exit(void){
+    tasklet_kill(&my_tasklet);
     free_irq(net_dev_irq_no, &handler_cookie);
 }
 
