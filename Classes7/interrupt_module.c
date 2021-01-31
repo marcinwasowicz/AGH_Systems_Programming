65b010f9ed3a7835a20aa8779639d2bc3064f29d
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-01-31 10:44:35 +0100

    implemented timer scheduling, still need to do more tests to see whether there are no bugs remaining

--- Classes7/interrupt_module.c
+++ Classes7/interrupt_module.c
@@ -1,17 +1,32 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
 
 MODULE_LICENSE("GPL");
 
 int handler_cookie;
 int net_dev_irq_no = 19;
 const char* net_dev_name = "ens33";
+
 struct tasklet_struct my_tasklet;
 unsigned long tasklet_data = 10;
 
+struct timer_list my_timer;
+unsigned long expires =  300;
+u32 timer_flags = 40;
+
+void timer_handler(struct timer_list* timer){
+    printk("Timer correctly executed\n");
+}
+
 void tasklet_handler(unsigned long data){
     printk("Tasklet executed with data: %ld\n", data);
+    int ret_val = mod_timer(&my_timer, jiffies + msecs_to_jiffies(expires));
+    if(ret_val){
+        printk("Timer is currently active, cannot schedule\n");
+    }
 }
 
 irqreturn_t handler(int interrupt_no, void* cookie){
@@ -21,21 +36,21 @@ irqreturn_t handler(int interrupt_no, void* cookie){
 }
 
 static int __init interrupt_init(void){
+    timer_setup(&my_timer, timer_handler, timer_flags);
+    tasklet_init(&my_tasklet, tasklet_handler, tasklet_data);
+    
     int result = request_irq(net_dev_irq_no, handler, IRQF_SHARED, net_dev_name, &handler_cookie);
 
     if(result){
         printk("Unable to register interrupt handler for NIC device\n");
     }
-    else{
-        tasklet_init(&my_tasklet, tasklet_handler, tasklet_data);
-    }
-
     return result;
 }
 
 static void __exit interrupt_exit(void){
-    tasklet_kill(&my_tasklet);
     free_irq(net_dev_irq_no, &handler_cookie);
+    tasklet_kill(&my_tasklet);
+    del_timer(&my_timer);
 }
 
 module_init(interrupt_init);
