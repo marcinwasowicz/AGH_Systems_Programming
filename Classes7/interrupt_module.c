26621e78084e4aff971c53d21b70d4b7d1364eea
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-02-03 09:57:51 +0100

    implemented task related to workqueue

--- Classes7/interrupt_module.c
+++ Classes7/interrupt_module.c
@@ -3,6 +3,7 @@
 #include <linux/interrupt.h>
 #include <linux/timer.h>
 #include <linux/jiffies.h>
+#include <linux/workqueue.h>
 
 MODULE_LICENSE("GPL");
 
@@ -16,6 +17,14 @@ unsigned long tasklet_data = 10;
 struct timer_list my_timer;
 unsigned long expires =  300;
 u32 timer_flags = 40;
+
+struct workqueue_struct* wq;
+const char* wq_name = "my_wq";
+struct work_struct wq_task;
+
+void wq_handler(struct work_struct* args){
+    printk("Task scheduled on workqueue executed correctly\n");
+}
 
 void timer_handler(struct timer_list* timer){
     printk("Timer correctly executed\n");
@@ -32,12 +41,24 @@ void tasklet_handler(unsigned long data){
 irqreturn_t handler(int interrupt_no, void* cookie){
     printk("NIC device sent interrupt\n");
     tasklet_schedule(&my_tasklet);
+
+    if(queue_work(wq, &wq_task)){
+        printk("Workqueue task already queued\n");
+    }
+
     return IRQ_NONE;
 }
 
 static int __init interrupt_init(void){
     timer_setup(&my_timer, timer_handler, timer_flags);
     tasklet_init(&my_tasklet, tasklet_handler, tasklet_data);
+
+    wq = create_singlethread_workqueue("my_wq");
+    if(!wq){
+        printk("Unalbe to create workqueue! not registering interrupt\n");
+        return -EFAULT;
+    }
+    INIT_WORK(&wq_task, wq_handler);
 
     int result = request_irq(net_dev_irq_no, handler, IRQF_SHARED, net_dev_name, &handler_cookie);
 
@@ -51,6 +72,7 @@ static void __exit interrupt_exit(void){
     free_irq(net_dev_irq_no, &handler_cookie);
     tasklet_kill(&my_tasklet);
     del_timer_sync(&my_timer);
+    destroy_workqueue(wq);
 }
 
 module_init(interrupt_init);
